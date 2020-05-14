fe900e762501af6ea3b20791eddb72635850ee0f
Scott Jehl <scottjehl@gmail.com>
2020-05-14 15:09:19 -0500

    simpler output

--- demo/testrun.js
+++ demo/testrun.js
@@ -2,5 +2,5 @@ var Iconoclash = require('../src/iconoclash');
 var fs = require('fs');
 var dir = fs.readdirSync("./svg", "utf-8");
 dir = dir.map(file => './svg/' + file);
-var iconoclash = new Iconoclash( dir, "./output/", { autoExpose: ["fill", "stroke"] } );
+var iconoclash = new Iconoclash( dir, "./output/", { autoExpose: ["fill"] } );
 iconoclash.process();
